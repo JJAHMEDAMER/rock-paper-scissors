439a1d02923775d0157a2212f52ae8acc133a27a
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-08 20:07:56 +0200

    While loop - try again

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from random import choice
 
 class rock_paper_scissor:
     def __init__(self) -> None:
-        player = input("Chose ['p' paper - 'r' rock - 's' scissors]: " )
-        robot = choice(['p', 's', 'r'])
-        print("You " + player, " VS ", "Robot " + robot)
-        if player == 'p' and robot == 'r' or player == 's' and robot == 'p' or player == 'r' and robot == 's':
-            print("YOU WIN!!!")
-        else:
-            print("Better Luck")
+        playing = 'y'
+        while playing == 'y':
+            player = input("Chose ['p' paper - 'r' rock - 's' scissors]: " )
+            robot = choice(['p', 's', 'r'])
+            print("You " + player, " VS ", "Robot " + robot)
+            if player == 'p' and robot == 'r' or player == 's' and robot == 'p' or player == 'r' and robot == 's':
+                print("YOU WIN!!!")
+            else:
+                print("Better Luck")
+            playing = input("Play Again?(y/n): ")
+                
 
 play = rock_paper_scissor()
